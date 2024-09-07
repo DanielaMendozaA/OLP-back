e6d56ed8bb070fcee3c2109e2d704635737ccde2
DanielaMendozaA <97774155+DanielaMendozaA@users.noreply.github.com>
2024-09-06 19:10:48 -0500

    modify cors

--- src/server.js
+++ src/server.js
@@ -1,8 +1,17 @@
 require('dotenv').config();
 const { pool } = require('./config/database');
 const app = require('./app');
+const cors = require('cors');
 
 const port = process.env.PORT || 4000;
+// const allowedOrigin = 'https://olp-front-test-r0msw0mr6-aarfees-projects.vercel.app/'
+
+// //Configure CORS
+// app.use(cors({
+//   origin: allowedOrigin,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// }))
 
 // Prueba la conexión a la base de datos
 pool.query('SELECT NOW()', (err, res) => {
